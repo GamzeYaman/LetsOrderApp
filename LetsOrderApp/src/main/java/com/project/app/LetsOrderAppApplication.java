e3bb448088f2b9e4965aa60926a3a1a389897a53
GamzeYaman <71887367+GamzeYaman@users.noreply.github.com>
2024-03-09 19:30:27 +0300

    The project has been started...

---
+++ LetsOrderApp/src/main/java/com/project/app/LetsOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsOrderAppApplication.class, args);
+	}
+
+}
